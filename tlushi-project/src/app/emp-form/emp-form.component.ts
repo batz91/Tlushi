cef8c63766de6f89bab073b6cd5ea21d80f66c01
erezam <amir4246@gmail.com>
2017-03-28 11:58:12 +0300

    upload

--- tlushi-project/src/app/emp-form/emp-form.component.ts
+++ tlushi-project/src/app/emp-form/emp-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
 
 @Component({
   selector: 'app-emp-form',
@@ -6,10 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./emp-form.component.css']
 })
 export class EmpFormComponent implements OnInit {
-
-  constructor() { }
-
+  
+  constructor() {
+    
+   }
+  
   ngOnInit() {
+      
   }
+  previewFile(){
+        var preview = document.querySelector('img'); //selects the query named img
+        var file;   // = document.querySelector('input[type=file]').files[0]; //sames as here
+        var reader  = new FileReader();
+        reader.onloadend = function () {
+            preview.src = reader.result;
+        }
 
+        if (file) {
+            reader.readAsDataURL(file); //reads the data as a URL
+        } else {
+            preview.src = "";
+        }
+   }
+
+   hello(){
+     alert("hello");
+   }
+ 
+
+ 
+ 
 }
