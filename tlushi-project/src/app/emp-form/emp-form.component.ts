c16e85dfa0905316f64469ac937ed26befb916e1
erezam <amir4246@gmail.com>
2017-03-28 12:52:32 +0300

    f

--- tlushi-project/src/app/emp-form/emp-form.component.ts
+++ tlushi-project/src/app/emp-form/emp-form.component.ts
@@ -14,19 +14,19 @@ export class EmpFormComponent implements OnInit {
   ngOnInit() {
       
   }
-  /*
   previewFile(){
         var preview = document.querySelector('img'); //selects the query named img
-        var file;   // = document.querySelector('input[type=file]').files[0]; //sames as here
+        var input: any = document.getElementById('fileInput');
+        var file = input.files[0];
         var reader  = new FileReader();
         reader.onloadend = function () {
             preview.src = reader.result;
         }
-
+       
         if (file) {
             reader.readAsDataURL(file); //reads the data as a URL
         } else {
             preview.src = "";
         }
-   }*/
+   }
 }
