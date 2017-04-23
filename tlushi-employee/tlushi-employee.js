6232849e61ce5a77d2a436339824bba8f6bad9d5
erezam <amir4246@gmail.com>
2017-04-23 10:41:55 +0300

    jsStart

---
+++ tlushi-employee/tlushi-employee.js
@@ -0,0 +1,19 @@
+var employeeAPI = function() {
+
+//	Create the initial appearance of the site
+	var initModule = function() {
+		fillForm();
+		$("#butCalc").click(calc);
+    };
+
+
+    var fillForm = function() {
+        return;
+    };
+
+    var calc = function() {
+        return;
+    }
+}();
+
+$(document).ready(employeeAPI.initModule);
