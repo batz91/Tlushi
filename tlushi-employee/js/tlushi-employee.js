9d2660f337e9da93a063d6c702798e585b5eb701
erezam <amir4246@gmail.com>
2017-04-25 14:02:28 +0300

    createOutput

--- tlushi-employee/js/tlushi-employee.js
+++ tlushi-employee/js/tlushi-employee.js
@@ -9,10 +9,7 @@ var employeeAPI = function() {
     };
 
     var calc = function() {
-        var fso = new ActiveXObject("Scripting.FileSystemObject");
-        var a = fso.CreateTextFile("c:\\testfile.txt", true);
-        a.WriteLine("This is a test.");
-        a.Close();
+        fillOutput();
     };
 
     var zoomIn = function(){
@@ -25,30 +22,66 @@ var employeeAPI = function() {
         $('#pic').height($('#pic').height()/1.2)
     }
 
-    var previewFile = function(){
-        var preview = document.getElementById('pic'); //selects the query named img
-        var input = document.getElementById('fileInput');
-        var file = input.files[0];
-        var reader  = new FileReader();
-        reader.onloadend = function () {
-            preview.src = reader.result;
-        }
-        if (file) {
-            reader.readAsDataURL(file); //reads the data as a URL
-        } else {
-            preview.src = "";
-        }
-   };
+    var fillOutput = function(){
+        var text = 
+            /*"<h1>תלוש השכר שלך במילים פשוטות </h1>"+
+            "</p>"+
+            "<h3>מה שמגיע לך</h3>"+
+            "</p>"+
+            "<label>תשלום על שעות נוספות</label>"+
+            "<div id='extraHoures'></div>"+
+            "<label>ימי חופשה</label>"+
+            "<div id='vication'></div>"+
+            "<label>דמי הבראה</label>"+
+            "<div id='health'></div>" ;
+        $("body").html(text);*/
+           
+            "<div class='output'>"+
+                "<img height='100' src='labelTlushi.jpg' width='200'>"+
+                "<h1>תלוש השכר שלך במילים פשוטות :</h1>"+
+                "<h2><p><u>מה שמגיע לך</u>:</p></h2>"+
+                "<p></p>"+
+                "<table>"+
+                    "<tr>"+
+                        "<th>"+
+                            "<img height='50' src='clock.png' width='50'>"+
+                            "<p>תשלום על שעות נוספות :</p>"+
+                            "<div id='extraHoures'></div>"+
+                            "<p><u></u></p>"+
+                        "</th>"+
+                        "<th>"+
+                            "<img height='50' src='plane.png' width='50'>"+
+                            "<p>ימי חופשה :</p>"+
+                            "<div id='vication'></div>"+
+                            "<p></p>"+
+                        "</th>"+
+                        "<th>"+
+                            "<img height='50' src='heart.png' width='50'>"+
+                            "<p>ימי הבראה :</p>"+
+                            "<div id='health'></div>" +
+                            "<p><u></u></p>"+
+                        "</th>"+
+                    "</tr>"+
+                "<table>"+
+                "<p>סיכום :</p>"+
+                "<div id='summery'></div>" +
+                "<p><u></u></p>"+
+                "<p></p>"+
+                "<p></p>"+
+                "<h2><p><u>מה קיבלת</u>:</p></h2>"+
+                "<p><u></u></p>"+
+                "<p><u></u></p>"+
+                "<h2><p><u>כדאי לבדוק:</u></p></h2>"+
+                "<p></p>"+
+                "<p></p>"+
+                "<button id = 'save' class='form-style-9'>שמור</button>"+
+            "</div>";
+        $("body").html(text);
+        $("#save").click(function(){document.getElementById('save').style.visibility='hidden'; 
+                                    window.print();
+                                    document.getElementById('save').style.visibility='visible'});
 
-   var WriteToFilefunction = function() {
-        var fso = CreateObject("Scripting.FileSystemObject");  
-        var s = fso.CreateTextFile("‏‏שולחן העבודה\test.txt", True);
-        s.writeline("HI");
-        s.writeline("Bye");
-        s.writeline("-----------------------------");
-        s.Close();
-        return;
-    };
+    }
 
     return {
         initModule : initModule,
