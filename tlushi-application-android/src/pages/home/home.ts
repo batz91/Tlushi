5d2640501dffaa29b7e8d1947d64024d5130547e
Itai Gafni <itaigafni@Itais-MacBook-Pro.local>
2017-04-27 13:02:46 +0300

    alert message

--- tlushi-application-android/src/pages/home/home.ts
+++ tlushi-application-android/src/pages/home/home.ts
@@ -14,38 +14,38 @@ export class HomePage {
   @Input() userEmail;
   @Input() userPhone;
 
-  constructor(private alertCtrl: AlertController, public navCtrl : NavController) {
-  }
+  constructor(private alertCtrl: AlertController, public navCtrl : NavController) {}
+
   showPicturePage(name, number, email) {
     this.userName = name.value;
     this.userEmail = email.value;
     this.userPhone = number.value;
     var nameFalg, emailFlag, userPhoneFlag;
-
+    var message ="";
     if(!(/^[a-z\u0590-\u05fe]+$/i.test(this.userName)))
      {
        nameFalg = true;
+       message+="נא למלא שם\n";
       }
     if(!(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(this.userEmail)))
       {
         emailFlag=true;
+        message+="נא למלא אימייל חוקי\n";
       }
     if(!(/^[05]+[0-9]{8}$/i.test(this.userPhone)) || !(/^[0]+[0-9]{8}$/i.test(this.userPhone)))
       {
           userPhoneFlag= true;
-      } 
-    if(this.userName == "" || this.userEmail == "" || this.userPhone == "")
-      return false;
-      let alert = this.alertCtrl.create({
+          message+="נא למלא טלפון חוקי\n"
+      }
+      if(message!=""){
+       let alert = this.alertCtrl.create({
        title: 'שדה חסר',
-       subTitle: 'נא למלא אימייל חוקי',
+       subTitle: message,
        buttons: ['אשר']
        });
-  alert.present();
+      alert.present();
       return false;
+    }
     this.navCtrl.push(Picture);
-}
-
-  
-  
+    }
 }
