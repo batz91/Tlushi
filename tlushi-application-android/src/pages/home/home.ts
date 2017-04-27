5af18b7709cdd4a8c0d86e682cd1cf9ccbbec158
Gal <Gal>
2017-04-27 10:26:31 +0300

    alerts

--- tlushi-application-android/src/pages/home/home.ts
+++ tlushi-application-android/src/pages/home/home.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import {NavController} from 'ionic-angular';
+import {AlertController} from 'ionic-angular';
 import {Picture} from '../picture/picture';
 
 
@@ -9,11 +10,38 @@ import {Picture} from '../picture/picture';
 })
 
 export class HomePage {
+  @Input() userName;
+  @Input() userEmail;
+  @Input() userPhone;
 
-
-  constructor(public navCtrl : NavController) {
+  constructor(private alertCtrl: AlertController, public navCtrl : NavController) {
   }
-  showPicturePage() {
+  showPicturePage(name, number, email) {
+    this.userName = name.value;
+    this.userEmail = email.value;
+    this.userPhone = number.value;
+    if(!(/^[a-z\u0590-\u05fe]+$/i.test(this.userName)))
+     {
+      let alert = this.alertCtrl.create({
+       title: 'שדה חסר',
+       subTitle: 'נא למלא שם מלא',
+       buttons: ['אשר']
+       });
+  alert.present();
+      return false;
+      }
+    if(!(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(this.userEmail)))
+      {
+      let alert = this.alertCtrl.create({
+       title: 'שדה חסר',
+       subTitle: 'נא למלא אימייל חוקי',
+       buttons: ['אשר']
+       });
+  alert.present();
+      return false;
+      }
+    if(this.userName == "" || this.userEmail == "" || this.userPhone == "")
+      return false;
     this.navCtrl.push(Picture);
 }
 
