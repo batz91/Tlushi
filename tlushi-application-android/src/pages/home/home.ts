543feed811db8614ed3e4873260df99a1c3d26b9
batz91 <galbatz@gmail.com>
2017-06-21 22:37:34 +0300

    fix 10

--- tlushi-application-android/src/pages/home/home.ts
+++ tlushi-application-android/src/pages/home/home.ts
@@ -1,20 +1,20 @@
-import { Component, Input } from "@angular/core";
-import {NavController} from "ionic-angular";
-import {AlertController} from "ionic-angular";
-import { ActionSheetController, NavParams, LoadingController } from "ionic-angular"
+import { Component, Input } from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {AlertController} from 'ionic-angular';
+import { ActionSheetController, NavParams, LoadingController } from 'ionic-angular'
 
-import { Camera } from "@ionic-native/camera";
-import { EndPage } from "../endpage/endpage";
+import { Camera } from '@ionic-native/camera';
+import { EndPage } from '../endpage/endpage';
 
 //  FireBase import
-import { AngularFire } from "angularfire/AngularFire";
-import {DomSanitizer} from "@angular/platform-browser";
-import { AngularFireDatabase, FirebaseListObservable } from "angularfire2/database";
-import * as firebase from "firebase";
+import { AngularFire } from 'angularfire/AngularFire';
+import {DomSanitizer} from '@angular/platform-browser';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import * as firebase from 'firebase';
 
 @Component({
-  selector: "page-home",
-  templateUrl: "home.html"
+  selector: 'page-home',
+  templateUrl: 'home.html'
 })
 
 export class HomePage {
@@ -26,107 +26,108 @@ export class HomePage {
   user: FirebaseListObservable<any>;
   terms: boolean;
 
-  constructor(public loadingCtrl:LoadingController ,public _DomSanitizationService:DomSanitizer, private navParams:NavParams, private af:AngularFireDatabase, private camera:Camera, public actionSheetCtrl:ActionSheetController, private alertCtrl:AlertController, public navCtrl :NavController) {
-    this.user = af.list("/user");
+  constructor(public loadingCtrl: LoadingController ,public _DomSanitizationService: DomSanitizer, private navParams: NavParams, private af: AngularFireDatabase, private camera: Camera, public actionSheetCtrl: ActionSheetController, private alertCtrl: AlertController, public navCtrl : NavController) {
+    this.user = af.list('/user');
     this.photoTaken = false;
-    this.terms= false;
+    this.terms=false;
   }
+
   showTerms(){
     let alert = this.alertCtrl.create({
-          cssClass: "buttons",
-          title: "תנאי שימוש",
-          message: "לחלחלחלחל",
-          buttons: ["אשר"]
+          cssClass:'buttons',
+          title: 'תנאי שימוש',
+          message: 'לחלחלחלחל',
+          buttons: ['אשר']
           });
           alert.present();
   }
-  updateTerms()
-  {
-      this.terms= !(this.terms); 
+  updateTerms(){
+      this.terms=!(this.terms); 
   }
-  showInfo()
-  {
+
+  showInfo(){
      let alert = this.alertCtrl.create({
-          cssClass: "buttons",
-          title: "מידע",
+          cssClass:'buttons',
+          title: 'מידע',
           message: "אלאלאלאלאלאה",
-          buttons: ["אשר"]
+          buttons: ['אשר']
           });
           alert.present(); 
+         
+        
   }
-  presentActionSheet() 
-  {
-    let actionSheet = this.actionSheetCtrl.create({
-    cssClass: "buttons",
-//     title: ":מקור תמונה",
+
+   presentActionSheet() {
+   let actionSheet = this.actionSheetCtrl.create({
+    cssClass: 'buttons',
+//     title: ':מקור תמונה',
      buttons: [
        {
-         cssClass: "buttons",
-         text: "מצלמה ",
+         cssClass: 'buttons',
+         text: 'מצלמה ',
          handler: () => {
             this.openCamera();
           }
        },
        {
-         cssClass: "buttons",
-         text: "גלריה ",
+         cssClass: 'buttons',
+         text: 'גלריה ',
          handler: () => {
            this.selectFromGallery();
          }
        },
        {
-         cssClass: "buttons",
-         text: "ביטול ",
-         role: "destructive",
+         cssClass: 'buttons',
+         text: 'ביטול ',
+         role: 'destructive',
          handler: () => {
-           console.log("Cancel clicked");
+           console.log('Cancel clicked');
          }
        }
      ]
    });
    actionSheet.present();
  }
- validation(email) 
- {
+
+  validation(name, number, email) {
+   
     this.userEmail = email.value;
-    let emailFlag;
-    let message = "";
+    var emailFlag;
+    var message ="";
     if(!(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(this.userEmail)))
-    {
-        emailFlag = true;
-        message += "<p> אימייל לא חוקי </p>";
-    }
-    if(message !== "")
-    {
+      {
+        emailFlag=true;
+        message+="<p> אימייל לא חוקי </p>";
+      }
+      if(message!=""){
           let alert = this.alertCtrl.create({
-            cssClass:"buttons",
-            title: "שדה חסר",
-            subTitle: message,
-            buttons: ["אשר"]
+          cssClass:'buttons',
+          title: 'שדה חסר',
+          subTitle: message,
+          buttons: ['אשר']
           });
           alert.present();
           return false;
-    }
-    if(this.terms === false)
-    {
+      }
+     if(this.terms==false)
+      {
         let alert = this.alertCtrl.create({
-              cssClass:"buttons",
-              subTitle: "יש לאשר את תנאי השימוש",
-              buttons: ["אשר"]
+              cssClass:'buttons',
+              subTitle: 'יש לאשר את תנאי השימוש',
+              buttons: ['אשר']
         });
         alert.present();
         return false;
      }
-     this.presentActionSheet();
+    this.presentActionSheet();
   }
-  openCamera() 
-  {
+  openCamera() {
     let loading = this.loadingCtrl.create({
-             content: "...התלוש בתהליך טעינה",
-             dismissOnPageChange: true
+             content: '...התלוש בתהליך טעינה',
+             dismissOnPageChange:true
     });
     loading.present();
-    let options = {
+    var options = {
       sourceType: this.camera.PictureSourceType.CAMERA,
       destinationType: this.camera.DestinationType.DATA_URL,
     };
@@ -141,10 +142,10 @@ export class HomePage {
         console.log(err);
     });
   }
-  selectFromGallery() 
-  {
+
+   selectFromGallery() {
     let loading = this.loadingCtrl.create({
-             content: "...התלוש בתהליך טעינה",
+             content: '...התלוש בתהליך טעינה',
              dismissOnPageChange:true
     });
     loading.present();
@@ -156,7 +157,7 @@ export class HomePage {
         encodingType: this.camera.EncodingType.PNG,
         mediaType: this.camera.MediaType.PICTURE,
         allowEdit: true,
-        correctOrientation: true  // Corrects Android orientation quirks
+        correctOrientation: true  //Corrects Android orientation quirks
     };
     this.camera.getPicture(options).then((imageData) => {
       this.cameraUrl = imageData;
@@ -168,19 +169,20 @@ export class HomePage {
         loading.dismiss();
         console.log(err);
     });
-  };
-  uploadObj() 
-  {
-      let image= this.cameraUrl;
-      if(this.cameraUrl === undefined)
-        image= "none";  
+  }
+
+   uploadObj() {
+      var image=  this.cameraUrl;
+      if(this.cameraUrl == undefined)
+        image= "none";
+    
     // firebase storage folder
     let storageRef = firebase.storage().ref();
     // Create a timestamp as filename
     const filename = Math.floor(Date.now() / 1000);
     // firebase upload image to storage
     storageRef.child(`paycheck/${this.userEmail}${filename}.png`)
-          .putString(image, "base64", { contentType: "image/png" }).then((savedPicture) => {
+          .putString(image, 'base64', { contentType: 'image/png' }).then((savedPicture) => {
     // create new user in DB
     this.user.push({
       email: this.userEmail, 
@@ -188,6 +190,8 @@ export class HomePage {
       status: "false"});
         });
 
-     this.navCtrl.push(EndPage, {userEmail: this.userEmail});
+     this.navCtrl.push(EndPage, {userEmail:this.userEmail});
+
   }
-};
+}
+
